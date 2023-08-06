7cf16c81fd8aa9d22431dc504adbc1cfd07c7123
Sun Xufei <xufei.sun@sap.com>
2023-08-06 19:19:32 +0800

    dev

---
+++ 04/dataset.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+
+def func(x):
+    if x>2:
+        return 2*x + 5
+    return 0
+
+
+x_train = []
+y_train = []
+for x in range(-10,10):
+    y = func(x)
+    x_train.append([x])
+    y_train.append([y])
+
+x_train = np.array(x_train, dtype=np.float32)
+y_train = np.array(y_train, dtype=np.float32)
+
+fig = plt.figure(figsize=(10, 5))
+plt.plot(x_train, y_train, 'ro', label='Original data')
+plt.legend() 
+plt.show()
+
+x_train = torch.from_numpy(x_train)
+y_train = torch.from_numpy(y_train)
+
+torch.save(x_train, 'datasets/train.pt')
+torch.save(y_train, 'datasets/train_target.pt')
